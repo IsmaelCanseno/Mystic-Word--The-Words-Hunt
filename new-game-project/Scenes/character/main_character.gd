fe349c2e43e21af1968dc9f66f16fae9ca6f09f9
solein <caloingkristhel18@gmail.com>
2025-02-07 11:27:44 +0800

    soundEffects

--- new-game-project/Scenes/character/main_character.gd
+++ new-game-project/Scenes/character/main_character.gd
@@ -4,6 +4,7 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 @onready var sprite_2d = $Sprite2D
+@onready var jumpSoundEffects = $JumpSoundEffects
 
 func _physics_process(delta: float) -> void:
 	# Animations 
@@ -20,6 +21,7 @@ func _physics_process(delta: float) -> void:
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		jumpSoundEffects.play()
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
